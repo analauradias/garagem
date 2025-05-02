4727aac65926d9b993bcab3850ba8d43a138f9b7
analauradias <analauramanfron@gmail.com>
2025-05-02 12:38:39 -0300

    feat: criação da API para acessorio

--- app/urls.py
+++ app/urls.py
@@ -8,9 +8,11 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 
 from core.views import UserViewSet
+from core.views.acessorio import AcessorioViewSet
 
 router = DefaultRouter()
 
+router.register(r'acessorios', AcessorioViewSet, basename='acessorios')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
