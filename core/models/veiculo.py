c18c81de6db66819214e146529956f3f4e349a09
analauradias <analauramanfron@gmail.com>
2025-05-02 13:18:40 -0300

    feat: criação da API para veiculo

---
+++ core/models/veiculo.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from .modelo import Modelo
+from .cor import Cor
+from .acessorio import Acessorio
+
+class Veiculo(models.Model):
+    ano = models.IntegerField(null=True, default=(0))
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=(0))
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
+    acessorio = models.ManyToManyField(Acessorio, related_name='livros', blank=True)
+
+
+    def __str__(self):
+        return f"{self.id} {self.modelo} {self.cor} {self.ano} "
